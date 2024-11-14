13e622639fe213eeca0b958f7061f60a1bc0d7ab
Louis6310 <120577455+Louis6310@users.noreply.github.com>
2024-11-14 13:33:20 +0100

    Update verif.sh

--- verif.sh
+++ verif.sh
@@ -3,8 +3,8 @@
 numero_a_trouver=$(cat nepasouvrir/num.txt)
 
 if [[ ! -e "$numero_a_trouver.txt" ]]; then
-    echo "Bravo ! Vous avez trouvé et supprimé le bon fichier ($numero_a_trouver.txt)."
+    echo "Bravo ! Vous avez trouvé et supprimé le bon fichier ($numero_a_trouver.txt). La bombe est désmorcée"
 else
-    echo "Ce n'est pas le bon fichier, ou vous n'avez pas encore supprimé de fichier. Essayez encore !"
+    echo "Ce n'est pas le bon fichier, ou vous n'avez pas encore supprimé de fichier. La bombe a explosée!"
 fi
 
